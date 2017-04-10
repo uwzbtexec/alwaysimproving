fa86bef90d428efea6f578dcdba11c6cc6458750
Alex Tan <alextan@uw.edu>
2017-04-10 16:39:44 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    title: 'Always Improving',
+    support: 'a scholarship by Zeta Beta Tau fraternity Alpha Mu Chapter at the University of Washington',
+    calltoaction: 'tell us your story',
+    intel: 'Intellectual Awareness',
+    social: 'Social Responsibility',
+    integ: 'Integrity',
+    broth: 'Brotherly Love'
+  }
+})
